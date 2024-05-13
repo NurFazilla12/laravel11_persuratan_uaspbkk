374ad27a37ced92ef0e68fff227427b42303705e
NurFazilla12 <zilla270523@gmail.com>
2024-05-14 01:06:37 +0700

    first commit

---
+++ database/migrations/2024_05_13_175032_create_surat_keluars_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('surat_keluars', function (Blueprint $table) {
+            $table->id();
+
+            $table->time('tanggal');
+            $table->string('no_surat');
+            $table->string('perihal');
+            $table->string('tujuan');
+            $table->text('isi_surat');
+            $table->foreignId('id_tandatangan')->references('id')->on('nama_tandatangans');
+            $table->foreignId('id_user')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('surat_keluars');
+    }
+};
